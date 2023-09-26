7a7c8348f59375daa4502b010eaad8a9c55c00d1
ABDALLAPC\Boda2 <boda2742001@gmail.com>
2023-09-26 03:51:10 +0300

    radio ui is done

--- lib/modules/radio/radio.dart
+++ lib/modules/radio/radio.dart
@@ -5,15 +5,27 @@ class Radioo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Radio",
-        style: TextStyle(
-            color: Colors.black,
-            fontSize: 50,
-            fontWeight: FontWeight.bold
-        ),
-      ),
+    var theme = Theme.of(context);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset("assets/images/radio logo.png"),
+        Text(
+          "إذاعة القرآن الكريم" ,
+          textAlign: TextAlign.center,
+          style:theme.textTheme.bodyMedium 
+          ,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.skip_previous , color: theme.primaryColor,size: 50,),
+            SizedBox(width: 20,),
+            Icon(Icons.play_arrow , color: theme.primaryColor,size: 50),
+            SizedBox(width: 20,),
+            Icon(Icons.skip_next , color: theme.primaryColor,size: 50,)
+          ],
+        )
+      ],
     );
   }
 }
